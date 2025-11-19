349308cf20a3b1bdd661932981633f5d4abb7883
Luca Bevivino <lucabevivino@Lucas-MacBook-Pro.local>
2025-11-19 18:43:49 +0100

    Sql-type enum, parsing correction, createSchema

--- create_db.js
+++ create_db.js
@@ -2,6 +2,17 @@ import mysql from 'mysql2/promise';
 const path = require('path');
 const Papa = require('papaparse');
 
+const SQL_TYPE_TABLE{
+  INTEGER: 'INT',
+  FLOAT: 'REAL',
+  DATE: 'DATE',
+  DATETIME: 'TIMESTAMP',
+  BOOLEAN: 'BOOLEAN'
+ }
+const SQL_TYPE = Object.freeze(SQL_TYPE_TABLE);
+
+
+// restituisce una tupla con il csv parsato a json e gli header
 export async function parseCsv(fileName){
 
   const filePath = path.join(filename);
@@ -12,24 +23,61 @@ export async function parseCsv(fileName){
     skipEmptyLines: true,
   };
 
-  Papa.parse(fileString, {
-    ...config,
-    complete: function(results){
-
-      const jsonData = results.data;
-      console.log("JSON Risultante:");
-      console.log(jsonData); // deve ritornare il json e le labels
-    }
+  return new Promise((resolve, reject)=>{
+        
+    Papa.parse(fileString, {
+      ...config,
+      complete: function(results){
+        if(results.errors.length > 0){
+          reject(new Error("Error during CSV parsing to Json: " + results.errors[0].message));
+          return;
+        }
+        const tuple = {
+          table: results.data,
+          fields: results.meta.fields
+          }
+        resolve(tuple);
+      },
+      error: function(err){
+        reject(err);
+      }
+    });
   });
+
 }
 
+// riceve la tupla json-array e determina le coppie campo-tipo
 export function createSchema(data){
 
-  // legge il json e crea uns secondo json dove associa ad ogni label un tipo SQL:
-  // int,float, VARCHAR[100];
+  let firstcheck = true;
+  let schema = [];
   
+  data.fields.forEach(field =>{
+    const type;
+    data.table.forEach(record =>{
+
+      const value = record[field];
+      if(firstcheck){
+         type = getType(value);
+         firstCheck = false;
+      }
+      else{
+        let tempType = getType(value);
+      }
+      if(!type.equal(tempType))
+        throw new Error('La tabella non è ben costruita, elementi della stessa colonna hanno fomrati diversi');      
+    })
+
+    const couple{
+      field: field,
+      type: type;
+    }
+    schema.add(couple);
+  })
+
+  return schema;
 }
-})
+
 
 export async function createTable(table, config, connection){
   // va aggiunta la configurazione della primary key e le relazioni tra tabelle
